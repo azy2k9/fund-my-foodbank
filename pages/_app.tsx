d6c31e3a899288557cdb192da59b717920a91c2b
evergreen-arslaan-qadus <74248128+evergreen-arslaan-qadus@users.noreply.github.com>
2022-06-18 11:58:48 +0100

    Fixed unused import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import "reset-css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
