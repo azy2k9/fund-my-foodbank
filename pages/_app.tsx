0bfa9734142e83e240af27d1fd13378ac1b03981
Ismail Mohamad <ikmohamad19@gmail.com>
2022-06-19 13:42:39 +0100

    removed coonatiner import to redmove conflict

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider, Container } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
